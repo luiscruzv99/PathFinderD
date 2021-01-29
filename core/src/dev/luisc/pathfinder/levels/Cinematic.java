691e4438031c30fe618392285502703a1de3f08e
Luis <luiscruzv99@gmail.com>
2021-01-29 19:49:56 +0100

    Created first 3 cinematics
    Still have to add the text, plus calculate the timings

--- core/src/dev/luisc/pathfinder/levels/Cinematic.java
+++ core/src/dev/luisc/pathfinder/levels/Cinematic.java
@@ -1,6 +1,30 @@
 package dev.luisc.pathfinder.levels;
 
-public class Cinematic {
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Cinematic implements RenderClass {
 
     //Composed of scenes
+    Menu m;
+    Scene1 scn1;
+    Scene2 scn2;
+    Scene3 scn3;
+    SpriteBatch batch;
+    RenderClass currentRender;
+    //Game's running at 60 fps, won't change that
+
+    public Cinematic (){
+        batch = new SpriteBatch();
+        m=new Menu();
+        scn3 = new Scene3(m,360,batch);
+        scn2 = new Scene2(scn3,360,batch);
+        scn1 = new Scene1(scn2, 180, batch);
+        currentRender = scn1;
+    }
+
+    @Override
+    public RenderClass render(OrthographicCamera c) {
+        return currentRender.render(c);
+    }
 }
---
+++ core/src/dev/luisc/pathfinder/levels/Scene5.java
@@ -0,0 +1,4 @@
+package dev.luisc.pathfinder.levels;
+
+public class Scene5 {
+}
